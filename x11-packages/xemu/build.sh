e79f585e6e1b459e02db2e97bc064c79f51330c4
Yew12347 <98322702+Yew12347@users.noreply.github.com>
2025-12-22 18:39:55 +0700

    Update build.sh

--- x11-packages/xemu/build.sh
+++ x11-packages/xemu/build.sh
@@ -1,17 +1,20 @@
 TERMUX_PKG_HOMEPAGE=https://xemu.app/
 TERMUX_PKG_DESCRIPTION="A free and open-source emulator for the original Xbox console."
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@George-Seven"
+TERMUX_PKG_MAINTAINER="@yewgamer"
 _COMMIT=956ef0b2ebe50896b7801d4f5ea621e431d9e3ae
 TERMUX_PKG_VERSION=0.8.5
 TERMUX_PKG_REVISION=1
+
 TERMUX_PKG_DEPENDS="at-spi2-core, brotli, fontconfig, freetype, fribidi, gdk-pixbuf, glib, harfbuzz, libandroid-shmem, libandroid-support, libbz2, libc++, libcairo, libdecor, libepoxy, libexpat, libffi, libgraphite, libiconv, libjpeg-turbo, libpcap, libpixman, libpng, libsamplerate, libslirp, libwayland, libx11, libxau, libxcb, libxcomposite, libxcursor, libxdamage, libxdmcp, libdecor, libxext, libxfixes, libxi, libxinerama, libxkbcommon, libxrandr, libxrender, libxss, mesa, openssl, pango, pcre2, sdl2, zlib"
+
 TERMUX_PKG_BUILD_DEPENDS="gtk3, libepoxy, libglvnd-dev, libpcap, libpixman, libsamplerate, libslirp, libtasn1, sdl2, vulkan-headers, xorgproto"
+
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_SHA256=08be4300e513bc36f91b3c8276ff2c9572c73dd8cf98fac04fc3a8233feef1cf
-TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686, x86_64"
+
 TERMUX_PKG_RM_AFTER_INSTALL="
 lib/python*
 "
@@ -21,114 +24,80 @@ lib/python*
 termux_step_get_source() {
 	mkdir -p "$TERMUX_PKG_SRCDIR"
 	cd "$TERMUX_PKG_SRCDIR"
-	git clone https://github.com/xemu-project/xemu
-	cd xemu
+	git clone https://github.com/xemu-project/xemu .
 	git checkout ${_COMMIT}
 	git submodule update --init --recursive
-	mv * .* ../
-}
-
-termux_step_post_get_source() {
-	local s=$(find . -type f ! -path '*/.git/*' -print0 \
-		| xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	[[ "$s" == "$TERMUX_PKG_SHA256  "* ]] \
-		|| termux_error_exit "Checksum mismatch"
 }
 
 # ---------------- PRE-CONFIGURE ----------------
 
 termux_step_pre_configure() {
-	# Workaround for https://github.com/termux/termux-packages/issues/12261.
-	if [ $TERMUX_ARCH = "aarch64" ]; then
-		rm -f $TERMUX_PKG_BUILDDIR/_lib
-		mkdir -p $TERMUX_PKG_BUILDDIR/_lib
+	if [ "$TERMUX_ARCH" = "aarch64" ]; then
+		rm -rf _lib
+		mkdir -p _lib _setjmp-aarch64/private
 
-		cd $TERMUX_PKG_BUILDDIR
-		mkdir -p _setjmp-aarch64
 		pushd _setjmp-aarch64
-		mkdir -p private
-		local s
 		for s in $TERMUX_PKG_BUILDER_DIR/setjmp-aarch64/{setjmp.S,private-*.h}; do
-			local f=$(basename ${s})
-			cp ${s} ./${f/-//}
+			cp "$s" .
 		done
 		$CC $CFLAGS $CPPFLAGS -I. setjmp.S -c
-		$AR cru $TERMUX_PKG_BUILDDIR/_lib/libandroid-setjmp.a setjmp.o
+		$AR cru ../_lib/libandroid-setjmp.a setjmp.o
 		popd
 
-		LDFLAGS+=" -L$TERMUX_PKG_BUILDDIR/_lib -l:libandroid-setjmp.a"
+		LDFLAGS+=" -L$PWD/_lib -l:libandroid-setjmp.a"
 	fi
 
-	termux_setup_cmake
+	termux_setup_meson
 	termux_setup_ninja
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
-		termux_setup_python_pip
-		pip install pyyaml
-	else
-		pip install --break-system-packages pyyaml
-	fi
+	termux_setup_python_pip
+	pip install pyyaml
 }
 
-# ---------------- CONFIGURE ----------------
+# ---------------- CONFIGURE (MESON) ----------------
 
 termux_step_configure() {
-	CFLAGS+=" -DANDROID -DEGL_NO_X11"
+	CFLAGS+=" -DANDROID -DEGL_NO_X11 -DXBOX=1"
 	CXXFLAGS+=" $CFLAGS"
 	LDFLAGS+=" -llog -landroid-shmem"
 
-	CONFIGURE_FLAGS="
-	--prefix=$TERMUX_PREFIX
-	--enable-egl
-	--disable-stack-protector
-	--disable-vte
-	--disable-vnc-sasl
-	--disable-xen
-	--disable-xen-pci-passthrough
-	--disable-hvf
-	--disable-whpx
-	--disable-snappy
-	--disable-lzfse
-	--disable-seccomp
-	--disable-vhost-user
-	--disable-vhost-user-blk-server
-	--disable-guest-agent
-	--disable-werror
-	--enable-trace-backends=nop
-	--target-list=i386-softmmu
-	--extra-cflags=-DXBOX=1
-	"
-
-	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
-		./configure $CONFIGURE_FLAGS
-	else
-		./configure \
-			--cross-prefix=${TERMUX_HOST_PLATFORM}- \
-			--host-cc=gcc \
-			--cc=$CC \
-			--cxx=$CXX \
-			--objcc=$CC \
-			$CONFIGURE_FLAGS
-	fi
-
-	grep -q CONFIG_EGL=y config-host.mak \
-		|| termux_error_exit "EGL not enabled"
-
-	grep -q CONFIG_VULKAN=y config-host.mak \
-		|| termux_error_exit "Vulkan not enabled"
-
-	! grep -q CONFIG_GLX=y config-host.mak \
-		|| termux_error_exit "GLX enabled (software path)"
+	meson setup build \
+		--prefix=$TERMUX_PREFIX \
+		--cross-file=$TERMUX_MESON_CROSSFILE \
+		-Ddefault_targets=i386-softmmu \
+		-Dopengl=enabled \
+		-Degl=enabled \
+		-Dglx=disabled \
+		-Dsdl=enabled \
+		-Dgtk=disabled \
+		-Dvte=disabled \
+		-Dvnc=disabled \
+		-Dvnc_sasl=disabled \
+		-Dxen=disabled \
+		-Dxen_pci_passthrough=disabled \
+		-Dhvf=disabled \
+		-Dwhpx=disabled \
+		-Dsnappy=disabled \
+		-Dlzfse=disabled \
+		-Dseccomp=disabled \
+		-Dvhost_user=disabled \
+		-Dvhost_user_blk_server=disabled \
+		-Dguest_agent=disabled \
+		-Dtrace_backends=nop \
+		-Dstack_protector=disabled \
+		-Dwerror=false \
+		-Db_c_args="-DXBOX=1 -DANDROID -DEGL_NO_X11" \
+		-Db_cpp_args="-DXBOX=1 -DANDROID -DEGL_NO_X11"
 }
 
 # ---------------- BUILD ----------------
 
 termux_step_make() {
-	make qemu-system-i386
+	ninja -C build qemu-system-i386
 
 	mkdir -p dist
 	mv build/qemu-system-i386 dist/xemu
 
-	python3 ./scripts/gen-license.py > dist/LICENSE.txt
+	python3 scripts/gen-license.py > dist/LICENSE.txt
 
 	sed "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
 		"$TERMUX_PKG_BUILDER_DIR/iso2xiso.in" > dist/iso2xiso
@@ -164,7 +133,7 @@ export SDL_RENDER_DRIVER=vulkan
 export VK_ICD_FILENAMES=$PREFIX/share/vulkan/icd.d/turnip_icd.json
 export MESA_LOADER_DRIVER_OVERRIDE=zink
 export GALLIUM_DRIVER=zink
-exec /data/data/com.termux/files/usr/libexec/xemu-bin -vulkan "$@"
+exec $PREFIX/libexec/xemu-bin -vulkan "$@"
 EOF
 	chmod +x "$TERMUX_PREFIX/bin/xemu"
 
@@ -175,5 +144,5 @@ EOF
 
 termux_step_install_license() {
 	install -Dm644 dist/LICENSE.txt \
-	"$TERMUX_PREFIX/share/doc/xemu/LICENSE"
+		"$TERMUX_PREFIX/share/doc/xemu/LICENSE"
 }
